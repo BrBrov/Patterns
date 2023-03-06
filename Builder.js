71bac48e6ec3c277e6a7de0d8c34952ba4f59e9c
brbrov <epoc@list.ru>
2023-03-06 22:39:36 +0300

    feat: add builder

---
+++ Builder.js
@@ -0,0 +1,33 @@
+let {log, dir} = console;
+
+class Point {
+	constructor (x) {
+		this.x = x;
+	}
+}
+
+class PointBuilder {
+	constructor(x) {
+		this.point = new Point(x);
+	}
+
+	addY (y) {
+		this.point.y = y;
+		return this;
+	}
+
+	addZ (z) {
+		this.point.z = z;
+		return this;
+	}
+
+	build() {
+		return this.point;
+	}
+}
+
+const point = new PointBuilder(1);
+
+const point1 = point.addY(2).addZ(3).build();
+
+log(point1);
