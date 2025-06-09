d26e69c0dfac19193200ac5810bad6d72cc9ba5a
BrBrov <epoc@list.ru>
2025-06-09 20:31:36 +0300

    fix: fix Prototype pattern

--- Prototype.js
+++ Prototype.js
@@ -1,31 +1,48 @@
 let {log, dir} = console;
 
-//Создаём копируем точки, что бы объекты были разнные по ссылке
+//Создаём - точки, что бы объекты могли возвращать свои копии
 
 class PointX {
-	constructor(x) {
-		this.x = x;
-	}
+    constructor(x) {
+        this.x = x;
+    }
+
+    clone() {
+        return new PointX(this.x);
+    }
 }
 
 class PointXY extends PointX{
-	constructor(x,y) {
-		super(x);
-		this.y = y;
-	}
+    constructor(x,y) {
+        super(x);
+        this.y = y;
+    }
+
+    clone() {
+        return new PointXY(this.x, this.y);
+    }
 }
 
 class PointXYZ extends PointXY {
-	constructor(x, y, z) {
-		super(x, y);
-		this.z = z;
-	}
-}
+    constructor(x, y, z) {
+        super(x, y);
+        this.z = z;
+    }
 
+    clone() {
+        return new PointXYZ(this.x, this.y, this.z);
+    }
+}
 
 const pointXY = new PointXY(1, 2);
 dir(pointXY);
 
 const pointXYZ = new PointXYZ(1, 2, 3);
-
 dir(pointXYZ);
+
+// Теперь можем создавать копии объектов
+const pointXYClone = pointXY.clone();
+dir(pointXYClone);
+
+const pointXYZClone = pointXYZ.clone();
+dir(pointXYZClone);
